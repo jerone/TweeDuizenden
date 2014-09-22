4a494e0fb4d96c0e0944e086e4837d22e82dfaf8
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-09-23 00:12:17 +0200

    fix

--- controllers/game.js
+++ controllers/game.js
@@ -1,27 +1,23 @@
-/**
- * GET /
- * Home page.
- */
-
 exports.index = function (req, res) {
-	res.render('game/index', { title: 'Game Index' });
+  res.render('game/index', { title: 'Games' });
 };
 
 exports.add = function (req, res) {
-	res.render('game/add', {
-		title: 'Game Add',
-		players: "Player 1\nPlayer 2\nPlayer 3"
-	});
+  res.render('game/add', {
+    title: 'Add Game',
+    name: new Date().getTime(),
+    players: "Player 1\nPlayer 2\nPlayer 3"
+  });
 };
 
 exports.start = function (req, res) {
-	if (typeof req.body.players !== "undefined") {
-		var players = req.body.players.split("\n");
-		res.render('game/start', {
-			title: 'Game Start',
-			players: players
-		});
-	} else {
-		res.redirect('/game/add');
-	}
+  if (typeof req.body.players !== "undefined") {
+    var players = req.body.players.split("\n");
+    res.render('game/start', {
+      title: 'Start Game',
+      players: players
+    });
+  } else {
+    res.redirect('/game/add');
+  }
 };
