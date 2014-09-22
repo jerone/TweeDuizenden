17d1206f42974df4966aae7086a41178177e5e8c
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-09-22 19:47:31 +0200

    fix

---
+++ public/js/tweeduizenden.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+  
+  var sum = function () {
+    var that = $(this);
+    var clas = that.data("player-index");
+    var row = that.parents("tr");
+    var prev = parseInt(row.prev().find(".g2000-player-" + clas).val(), 10);
+    var preprev = parseInt(row.prev().prev().find(".g2000-player-" + clas).val(), 10);
+    //console.log(row, clas, prev, preprev);
+    if (!isNaN(prev) && !isNaN(preprev)) {
+      that.val(prev + preprev);
+    } else {
+      that.val("");
+    }
+  };
+  
+  $(".g2000-value").keyup(function () {
+    $(".g2000-sum").each(sum);
+  });
+
+
+});
