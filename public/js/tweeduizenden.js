2240122b70728e95ee87a189cd8c7ccdee282d18
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-09-22 22:21:38 +0200

    fix

--- public/js/tweeduizenden.js
+++ public/js/tweeduizenden.js
@@ -1,12 +1,11 @@
 ﻿$(function () {
-  
+
   var sum = function () {
     var that = $(this);
-    var clas = that.data("player-index");
+    var index = that.data("player-index");
     var row = that.parents("tr");
-    var prev = parseInt(row.prev().find(".g2000-player-" + clas).val(), 10);
-    var preprev = parseInt(row.prev().prev().find(".g2000-player-" + clas).val(), 10);
-    //console.log(row, clas, prev, preprev);
+    var prev = parseInt(row.prev().find(".g2000-player-" + index).val(), 10);
+    var preprev = parseInt(row.prev().prev().find(".g2000-player-" + index).val(), 10);
     if (!isNaN(prev) && !isNaN(preprev)) {
       that.val(prev + preprev);
     } else {
@@ -15,8 +14,9 @@
   };
   
   $(".g2000-value").keyup(function () {
-    $(".g2000-sum").each(sum);
+    var that = $(this);
+    var index = that.data("player-index");
+    $(".g2000-sum.g2000-player-" + index).each(sum);
   });
-
 
 });
