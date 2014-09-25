2c2bf5932a4cb7153f4646ae24624ef4eefdb21f
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-09-25 20:15:24 +0200

    Trigger sum when game is loaded;

--- public/js/tweeduizenden.js
+++ public/js/tweeduizenden.js
@@ -1,11 +1,11 @@
 ﻿$(function () {
 
   var sum = function () {
-    var that = $(this);
-    var index = that.data("player-index");
-    var row = that.parents("tr");
-    var prev = parseInt(row.prev().find(".g2000n-player-" + index).val(), 10);
-    var preprev = parseInt(row.prev().prev().find(".g2000n-player-" + index).val(), 10);
+    var that = $(this),
+        index = that.data("player-index"),
+        row = that.parents("tr"),
+        prev = parseInt(row.prev().find(".g2000n-player-" + index).val(), 10),
+        preprev = parseInt(row.prev().prev().find(".g2000n-player-" + index).val(), 10);
     if (!isNaN(prev) && !isNaN(preprev)) {
       that.val(prev + preprev);
     } else {
@@ -14,9 +14,7 @@
   };
   
   $(".g2000n-value").keyup(function () {
-    var that = $(this);
-    var index = that.data("player-index");
-    $(".g2000n-sum.g2000n-player-" + index).each(sum);
-  });
+    $(".g2000n-sum.g2000n-player-" + $(this).data("player-index")).each(sum);
+  }).trigger("keyup");
 
 });
