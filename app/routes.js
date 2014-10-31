0531baa9189add1e128f3e1803caf65202f24d9c
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-10-31 16:06:08 +0100

    Better 404 responses;

--- app/routes.js
+++ app/routes.js
@@ -40,20 +40,27 @@ module.exports = function routes(app) {
   app.route('/game/api').get(gameController.api);
 
   // Handle 404;
-  app.use(function (req, res) {
-    res.status(400).render('error', {
-      title: req.i18n.t('error:404.title'),
-      code: 404,
-      message: req.i18n.t('error:404.message')
-    });
+  app.use(function (req, res/*, next*/) {
+    res.status(404);
+    if (req.accepts('html')) {
+      res.render('error', {
+        title: req.i18n.t('error:404.title'),
+        code: 404,
+        message: req.i18n.t('error:404.message')
+      });
+    } else if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+    } else {
+      res.type('txt').send('Not found');
+    }
   });
 
   // Handle 500;
-  app.use(function (error, req, res, next) {
-    res.status(500).render('error', {
+  app.use(function (err, req, res/*, next*/) {
+    res.status(err.status || 500).render('error', {
       title: '500: Internal Server Error',
       code: 505,
-      message: error
+      message: err
     });
   });
 
